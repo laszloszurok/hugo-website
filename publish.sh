c1b6f4302f46a8317b1f07f7705e1868304a0893
László Szurok <48314396+laszloszurok@users.noreply.github.com>
2025-08-05 21:21:30 +0200

    add pagefind reindexing to publish.sh

--- publish.sh
+++ publish.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
+set -o errexit
+
 echo "Generating static files..."
 hugo --quiet
+
+if [ ! -d venv ]; then
+    echo "Installing pagefind in a python virtual environment..."
+    python3 -m venv venv
+    pip install 'pagefind[extended]'
+fi
+
+echo "Activating python virtual environment..."
+. venv/bin/activate
+
+echo "Generating search index with pagefind..."
+python3 -m pagefind --site public
 
 echo "Archiving static files for upload..."
 tar czf public.tar.gz public
